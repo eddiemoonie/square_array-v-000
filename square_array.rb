74d1032d53c762898af2e7ca5897b4159771921a
Eddie Moon <eddiemoonie@gmail.com>
2018-02-13 22:03:47 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,10 @@
 def square_array(array)
+  squared = []
+  array.each do |number|
+    squared.push(number * number)
+  end
+  squared
 #  squared = []
-#  array.each do |number|
-#    squared.push(number * number)
-#  end
+#  array.collect { |number| squared << number ** 2 }
 #  squared
-  square = []
-  array.collect { |number| square << number ** 2 }
-  square
 end
